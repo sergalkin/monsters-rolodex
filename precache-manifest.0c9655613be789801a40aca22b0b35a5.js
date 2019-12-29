a51a90832aa1c3aa29282dcf725bad1b1c1ab8a4
Siv <sa.galkin94@gmail.com>
2019-12-30 01:31:11 +0300

    Updates

---
+++ precache-manifest.0c9655613be789801a40aca22b0b35a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fd8c44bd748f2870c6df061c9779ea5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "dfba369296a1eefda752",
+    "url": "/monsters-rolodex/static/css/main.4aea1a24.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dfba369296a1eefda752",
+    "url": "/monsters-rolodex/static/js/main.005022ca.chunk.js"
+  },
+  {
+    "revision": "6e191d8baec223c78143",
+    "url": "/monsters-rolodex/static/js/runtime-main.c1ee7b67.js"
+  }
+]);
